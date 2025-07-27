eaaf6694f7fc94a24374fa0380570fa570c57812
Anh-Khanh <leanhkhanh6540@gmail.com>
2025-07-27 21:56:26 +0700

    init

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,80 +14,104 @@ class LoginPage extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Typography("Welcome to"),
+            SizedBox(width: 4),
             Typography("TodoApp", color: AppColors.dartBrand),
           ],
         ),
       ),
-      child: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 600),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(height: 30),
-                Image.asset(
-                  "assets/images/login.png",
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-                CustomButton(
-                  text: "Continue",
-                  fontWeight: FontWeight.w600,
-                  isFilled: true,
-                  backgroundColor: AppColors.dartSuccess,
-                  textColor: AppColors.backgroundNeutral,
-                  onPressed: () async {},
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: Container(height: 1, color: AppColors.lineNeutral),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0,
-                      ), // 👈 khoảng cách hai bên
-                      child: Typography(
-                        "or continue with",
-                        color: AppColors.secondaryNeutral,
-                        size: 12,
+      child: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          const SizedBox(height: 32),
+                          Image.asset(
+                            "assets/images/login.png",
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                          const SizedBox(height: 32),
+                        ],
                       ),
-                    ),
-                    Expanded(
-                      child: Container(height: 1, color: AppColors.lineNeutral),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: CustomButton(
-                        text: "Facebook",
-                        isFilled: true,
-                        icon: Icon(CupertinoIcons.flag_circle),
-                        backgroundColor: AppColors.buttonGray,
+
+                      // Phần cuối trang
+                      Column(
+                        children: [
+                          CustomButton(
+                            text: "Continue",
+                            fontWeight: FontWeight.w600,
+                            isFilled: true,
+                            backgroundColor: AppColors.dartSuccess,
+                            textColor: AppColors.backgroundNeutral,
+                            onPressed: () async {},
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  height: 1,
+                                  color: AppColors.lineNeutral,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16.0,
+                                ),
+                                child: Typography(
+                                  "or continue with",
+                                  color: AppColors.secondaryNeutral,
+                                  size: 12,
+                                ),
+                              ),
+                              Expanded(
+                                child: Container(
+                                  height: 1,
+                                  color: AppColors.lineNeutral,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: CustomButton(
+                                  text: "Facebook",
+                                  isFilled: true,
+                                  icon: Icon(CupertinoIcons.flag_circle),
+                                  backgroundColor: AppColors.buttonGray,
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                              Expanded(
+                                child: CustomButton(
+                                  text: "Google",
+                                  isFilled: true,
+                                  backgroundColor: AppColors.buttonGray,
+                                  icon: Icon(CupertinoIcons.globe),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 30),
+                        ],
                       ),
-                    ),
-                    SizedBox(width: 20),
-                    Expanded(
-                      child: CustomButton(
-                        text: "Google",
-                        isFilled: true,
-                        backgroundColor: AppColors.buttonGray,
-                        icon: Icon(CupertinoIcons.globe),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
